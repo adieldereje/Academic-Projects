8c200aaa862e99171d0ff75ab3741c5257ebc36b
adieldereje <137972745+adieldereje@users.noreply.github.com>
2023-06-27 23:19:57 -0400

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,33 @@
+'''
+Project 3 - Text Adventure - Spring 2023  
+Author: John Lewis - lewis63
+
+Modified by:  <your name and id here>
+
+The program presents a text-based adventure game with
+multiple rooms and items.
+'''
+
+from game import Game
+
+
+def main():
+
+    game = Game()
+    
+    finished = False
+    
+    while not finished:
+        print()
+        command_input = input('> ')
+        print()
+        command = game.clean_command(command_input)
+        if command == 'quit':
+            finished = True
+        else:
+            game.process_command(command)    
+
+
+# Call main like this to keep Web-CAT happy:
+if __name__ == '__main__':
+    main()
